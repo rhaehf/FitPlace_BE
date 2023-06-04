b8f6e95b89e180106e154fcb3513e3bbf5c42df9
Jinhui <jda0526@naver.com>
2023-06-04 19:50:16 +0900

    setting file

---
+++ server/src/test/java/gaefourdong/fitplace/FitplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package gaefourdong.fitplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
